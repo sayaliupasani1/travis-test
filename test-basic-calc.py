09f4283179df10ffb086df25f8d18a8036214719
Sayali Upasani <sayaliupasani@gmail.com>
2019-07-04 11:06:38 -0700

    Sample test change

--- test-basic-calc.py
+++ test-basic-calc.py
@@ -3,7 +3,7 @@ import calculator
 class TestCalc:
 
 	def test_addition(self):
-		assert 5 == calculator.add(2,2,1)
+		assert 6 == calculator.add(2,2,2)
 
 	def test_sub(self):
 		assert 2 == calculator.sub(4,2)
