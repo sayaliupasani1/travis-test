a62cc27c68c96027222d894bc663e09a1afa5d51
Mohit <Mohitsharma44@gmail.com>
2019-07-04 10:53:41 -0700

    Added docstrings

--- calculator.py
+++ calculator.py
@@ -1,11 +1,40 @@
 def add(num1, num2, num3):
+	"""
+	Function to add three numbers
+	Parameters
+	----------
+	num1: int, float
+	num2: int, float
+	num3: int, float
+	"""
 	return num1 + num2 + num3
 
 def sub(num1, num2):
+	"""
+	Function to subtract two numbers:
+	Parameters
+	----------
+	num1: int, float
+	num2: int, float
+	"""
 	return num1 - num2
 
 def multiply(num1, num2):
+	"""
+	Function to multiply two numbers
+	Parameters
+	----------
+	num1: int, float
+	num2: int, float
+	"""
 	return num1 * num2
 
 def divide(num1, num2):
+	"""
+	Function to divide two numbers
+	Parameters
+	----------
+	num1: int, float
+	num2: int, float
+	"""
 	return num1/num2
